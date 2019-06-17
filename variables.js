0ed33bcdec13e86f3e9ad68c033d1f46d5403367
Dustin Fedie <dlfedie@gmail.com>
2019-06-17 18:05:07 -0500

    Changing vars

--- variables.js
+++ variables.js
@@ -3,3 +3,17 @@ console.log('js');
 let testVar = 9;
 
 console.log(testVar);
+
+console.log('The value of testVar is', testVar);
+
+testVar = 1.61803398875;
+
+console.log('The value of testVar is', testVar);
+
+testVar = 'the kitty is hungry';
+
+console.log('The value of testVar is', testVar);
+
+testVar = false;
+
+console.log('The value of testVar is', testVar);
