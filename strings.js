712e32217e15a2c211e65eefbd930cf8ef7bc2a6
Dustin Fedie <dlfedie@gmail.com>
2019-06-18 14:11:39 -0500

    made some changes to the strings.js file

--- strings.js
+++ strings.js
@@ -1,1 +1,21 @@
 console.log('js');
+
+let catName = 'Mae Roo';
+let catAge = 3;
+console.log(catName);
+// concatination with comma for console (only works in console.log())
+console.log('The name of the cat is:', catName);
+console.log('The age of the cat is', catAge);
+
+// concatinate text with "+"
+let sentence = 'This is a sentence about a cat named ' + catName;
+console.log(sentence);
+
+sentence = 'The cat is ' + catAge + ' years old.'
+console.log(sentence);
+
+catAge = 9;
+catName = 'Josie';
+
+sentence = 'At another house, there is a ' + catAge + ' year old cat named ' + catName + '.';
+console.log(sentence);
