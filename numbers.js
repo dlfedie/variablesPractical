a0aa927e016541e70acac5dca455f8f973d844e0
Dustin Fedie <dlfedie@gmail.com>
2019-06-18 13:44:41 -0500

    Played with number vars and cars

---
+++ numbers.js
@@ -0,0 +1,41 @@
+console.log('js');
+
+let motorbikes = 9;
+let cars = 0;
+let vehicles = motorbikes + cars;
+console.log('bikes:', motorbikes);
+console.log('cars:', cars);
+console.log('vehicles', vehicles);
+
+console.log('had to put the mio in the garage');
+
+motorbikes--;
+
+console.log('bikes:', motorbikes);
+console.log('cars:', cars);
+vehicles = motorbikes + cars;
+console.log('vehicles', vehicles);
+
+console.log('missed the mio so bought 2 wrx\'s');
+cars+=2;
+console.log('bikes:', motorbikes);
+console.log('cars:', cars);
+vehicles = motorbikes + cars;
+console.log('vehicles', vehicles);
+
+console.log('wanted a Jetta so I got one');
+cars++;
+console.log('sold 3 bikes');
+motorbikes-= 3;
+console.log('bikes:', motorbikes);
+console.log('cars:', cars);
+vehicles = motorbikes + cars;
+console.log('vehicles', vehicles);
+
+console.log('got the mio back from the garage. it is as lit as their ad campaign thinks it is!');
+motorbikes++;
+
+console.log('bikes:', motorbikes);
+console.log('cars:', cars);
+vehicles = motorbikes + cars;
+console.log('vehicles', vehicles);
