f8fbc1186723c1f7a0c903e8d081f28887dea4a4
Dustin Fedie <dlfedie@gmail.com>
2019-06-19 20:59:59 -0500

    Trying out devtools

---
+++ devtools.js
@@ -0,0 +1,18 @@
+console.log('js');
+
+let firstNumber = 3.141;
+let secondNumber = 1.618;
+let awesome = true;
+let dumb =  false;
+
+let answer = firstNumber + secondNumber;
+
+console.log(answer);
+
+console.log('the answer is ' + answer + " " + awesome);
+
+console.log('the answer is', answer, awesome);
+
+console.log(awesome);
+
+console.log(dumb);
